d853f261aef653b38d796770d864c8738e8bb3b4
mj.jeon <mj.jeon@how2marry.com>
2019-08-01 16:07:54 +0900

    initial commit

---
+++ BasicWeatherApp/BasicWeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BasicWeatherApp
+//
+//  Created by MIJIN JEON on 01/08/2019.
+//  Copyright © 2019 MIJIN JEON. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
