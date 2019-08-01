5ecf8dfc56cf983700cadabb5b050d2141468a47
Mijin Jeon <mayihelpu27@gmail.com>
2019-08-02 01:15:39 +0900

    Feat: Add FavoriteCityTableViewController, FavoriteCityTableViewCell

---
+++ BasicWeatherApp/BasicWeatherApp/FavoriteCityTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FavoriteCityTableViewCell.swift
+//  BasicWeatherApp
+//
+//  Created by MIJIN JEON on 01/08/2019.
+//  Copyright © 2019 MIJIN JEON. All rights reserved.
+//
+
+import UIKit
+
+class FavoriteCityTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ BasicWeatherApp/BasicWeatherApp/Weather.swift
@@ -0,0 +1,9 @@
+//
+//  Weather.swift
+//  BasicWeatherApp
+//
+//  Created by MIJIN JEON on 01/08/2019.
+//  Copyright © 2019 MIJIN JEON. All rights reserved.
+//
+
+import Foundation
