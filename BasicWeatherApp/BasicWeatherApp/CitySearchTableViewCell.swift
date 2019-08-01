070ef6ae1b4404f72b6b08e31fb925d565da5712
Mijin Jeon <mayihelpu27@gmail.com>
2019-08-01 16:17:45 +0900

    feat: Add tableView cell file

---
+++ BasicWeatherApp/BasicWeatherApp/CitySearchTableViewCell.swift
@@ -0,0 +1,33 @@
+//
+//  CitySearchTableViewCell.swift
+//  BasicWeatherApp
+//
+//  Created by MIJIN JEON on 01/08/2019.
+//  Copyright © 2019 MIJIN JEON. All rights reserved.
+//
+
+import UIKit
+
+class CitySearchTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var cityName: UILabel!
+    
+    var city: String? {
+        didSet {
+            guard let city = self.city else { return }
+            self.cityName.text = city
+        }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+
+}
