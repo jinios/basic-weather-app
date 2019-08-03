cbbd2cfda02108791d44cab69c2256a32322a061
Mijin Jeon <mayihelpu27@gmail.com>
2019-08-04 00:05:48 +0900

    feat: Add WeatherSummaryTableViewCell file

---
+++ BasicWeatherApp/BasicWeatherApp/TableViewCell/WeatherSummaryTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  WeatherSummaryTableViewCell.swift
+//  BasicWeatherApp
+//
+//  Created by YOUTH2 on 03/08/2019.
+//  Copyright © 2019 MIJIN JEON. All rights reserved.
+//
+
+import UIKit
+
+class WeatherSummaryTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
