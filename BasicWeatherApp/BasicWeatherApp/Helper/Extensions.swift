03e238b9b01d781432030ccfbe73d98b1a6cfd41
Mijin Jeon <mayihelpu27@gmail.com>
2019-08-04 23:05:29 +0900

    feat: Implement extension to convert Date to String

--- BasicWeatherApp/BasicWeatherApp/Helper/Extensions.swift
+++ BasicWeatherApp/BasicWeatherApp/Helper/Extensions.swift
@@ -17,3 +17,55 @@ extension Data {
         return prettyPrintedString
     }
 }
+
+extension Date {
+
+    func convertToString(format: String) -> String {
+        let formatter = DateFormatter.init()
+        formatter.dateFormat = format
+        return formatter.string(from: self)
+    }
+
+    func convertHour() -> Int {
+        return Int(self.convertToString(format: "HH")) ?? 0
+    }
+
+    func convertMinute() -> Int {
+        return Int(self.convertToString(format: "mm")) ?? 0
+    }
+
+    func convertAMPM() -> String {
+        let hour = self.convertHour()
+        return hour < 12 ? "오전" : "오후"
+
+    }
+
+    func convertAMPMHHMM() -> String {
+        var result : String = ""
+        result.append(self.convertAMPM())
+
+        if self.convertHour() > 12 {
+            result.append(" \(self.convertHour()-12)")
+        } else {
+            result.append(" \(self.convertHour())")
+        }
+
+        result.append(":\(self.convertMinute())")
+
+        return result
+    }
+}
+
+extension String {
+    var convertDate : Date? {
+        get {
+            let formatter: DateFormatter = DateFormatter()
+            formatter.timeZone = TimeZone.init(abbreviation: "KST")
+            formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+
+            guard let date = formatter.date(from: self) else { return nil }
+            return date
+        }
+    }
+
+}
