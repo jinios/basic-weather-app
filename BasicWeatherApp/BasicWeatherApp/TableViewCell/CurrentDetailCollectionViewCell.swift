51030b82c52c48d10dd444b7a9b170071626ba7a
Mijin Jeon <mayihelpu27@gmail.com>
2019-08-04 23:07:47 +0900

    feat: Implement CurrentDetailList struct
    To manage MiscellaneousDetailWeather, and Implement CurrentDetailCollectionView Datasource

--- BasicWeatherApp/BasicWeatherApp/TableViewCell/CurrentDetailCollectionViewCell.swift
+++ BasicWeatherApp/BasicWeatherApp/TableViewCell/CurrentDetailCollectionViewCell.swift
@@ -9,5 +9,56 @@
 import UIKit
 
 class CurrentDetailCollectionViewCell: UICollectionViewCell {
-    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var infoLabel: UILabel!
+
+    var detailInfo: MiscellaneousDetailWeather? {
+        didSet {
+            self.set()
+        }
+    }
+
+    func set() {
+        guard let detailInfo = self.detailInfo else { return }
+        self.titleLabel.text = detailInfo.title
+        self.infoLabel.text = detailInfo.value
+    }
+
+}
+
+protocol MiscellaneousDetailWeather {
+    var title: String { get }
+    var value: String { get }
+}
+
+struct CurrentDetailList {
+
+    private let currentWeather: CurrentWeather
+    private var miscellaneousWeatherList: [MiscellaneousDetailWeather] = []
+
+    var count: Int {
+        return miscellaneousWeatherList.count
+    }
+
+    init(currentWeather: CurrentWeather) {
+        self.currentWeather = currentWeather
+        makeMiscellaneousWeatherList()
+    }
+
+    private mutating func makeMiscellaneousWeatherList() {
+        let details: [MiscellaneousDetailWeather?] =
+            [currentWeather.clouds,
+             currentWeather.detailWeather.humidity,
+             currentWeather.rain,
+             currentWeather.snow,
+             currentWeather.wind,
+             currentWeather.detailWeather.pressure]
+
+        miscellaneousWeatherList = details.compactMap { $0 }
+    }
+
+    func miscellaneousDetail(at index: Int) -> MiscellaneousDetailWeather {
+        return miscellaneousWeatherList[index]
+    }
+
 }
